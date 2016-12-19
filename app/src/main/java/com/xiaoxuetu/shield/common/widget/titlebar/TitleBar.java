cabb39357726c20978b40b968e0d1220ecdd0602
kevin <hjj20040849@qq.com>
2016-12-20 01:18:52 +0800

    FEATURE: 完成无设备连接时路由的显示UI图 1、添加下拉刷新框架的依赖 2、添加无设备连接时路路由的显示UI 3、暂时屏蔽多路由设别的支持图标

--- app/src/main/java/com/xiaoxuetu/shield/common/widget/titlebar/TitleBar.java
+++ app/src/main/java/com/xiaoxuetu/shield/common/widget/titlebar/TitleBar.java
@@ -56,14 +56,14 @@ public class TitleBar extends FrameLayout {
         initReturnImageViewAction();
     }
 
-    private static int getStatusBarHeight(Context context) {
-        int result = 0;
-        int resourceId = context.getResources().getIdentifier(
-                "status_bar_height", "dimen", "android");
-
-        if (resourceId > 0) {
-            result = context.getResources().getDimensionPixelSize(resourceId);
-        }
-        return result;
-    }
+//    private static int getStatusBarHeight(Context context) {
+//        int result = 0;
+//        int resourceId = context.getResources().getIdentifier(
+//                "status_bar_height", "dimen", "android");
+//
+//        if (resourceId > 0) {
+//            result = context.getResources().getDimensionPixelSize(resourceId);
+//        }
+//        return result;
+//    }
 }

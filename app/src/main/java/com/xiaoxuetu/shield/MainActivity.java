4151edca73801e666d3b4b69cc3d0c367abf147f
kevin <hjj20040849@qq.com>
2016-12-27 01:16:41 +0800

    FEATURE:添加设备列表显示 1、存在一个bug：设别名称无法正常显示出来，待解决 2、修正由于RelativeLayout导致设备显示时，titlebar显示异常的情况

--- app/src/main/java/com/xiaoxuetu/shield/MainActivity.java
+++ app/src/main/java/com/xiaoxuetu/shield/MainActivity.java
@@ -1,8 +1,18 @@
 package com.xiaoxuetu.shield;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
+
+import com.xiaoxuetu.shield.common.widget.dialog.MLTextView;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -25,21 +35,79 @@ public class MainActivity extends AppCompatActivity {
 
     private void initListView() {
         listView = (ListView) findViewById(R.id.client_list_view);
+        List<Map<String, Object>> list = getMockData();
+        ListAdapter adapter = new ClientListAdapter(this, list);
+        listView.setAdapter(adapter);
 
 
     }
-
     private List<Map<String, Object>> getMockData() {
+
         List<Map<String, Object>> mockData = new ArrayList<>();
 
-        Map<String, Object> data1 = new HashMap<>();
-        data1.put("client_icon", R.drawable.client_device_list_unknown);
-        data1.put("client_name", "XiaoMi");
-        data1.put("client_event", "5G连接");
-        data1.put("event_time", "2016-12-24 00:10:12");
-        data1.put("client_net_speed", "1.9 KB/s");
+        for (int i=0; i<5; i++) {
+            Map<String, Object> data1 = new HashMap<>();
+            data1.put("client_icon", R.drawable.client_device_list_unknown);
+            data1.put("client_name", "XiaoMi-" + i);
+            data1.put("client_event", "5G连接");
+            data1.put("event_time", "2016-12-24 00:10:1" + i);
+            data1.put("client_net_speed", "1.9 KB/s");
 
-        mockData.add(data1);
+            mockData.add(data1);
+        }
+
         return mockData;
+    }
+
+    public class ClientListAdapter extends BaseAdapter {
+
+        private LayoutInflater inflater;
+
+        private  List<Map<String, Object>> dataList;
+
+        public ClientListAdapter(Context context, List<Map<String, Object>> dataList) {
+            this.inflater = LayoutInflater.from(context);
+            this.dataList = dataList;
+        }
+
+        @Override
+        public int getCount() {
+            return dataList.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return dataList.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            Map<String, Object> currentDataMap = dataList.get(position);
+            convertView = inflater.inflate(R.layout.client_list_device_item, null);
+            // 设置客户端图片
+            ((ImageView) convertView.findViewById(R.id.client_icon))
+                    .setImageResource((int)(currentDataMap.get("client_icon")));
+
+            ((MLTextView) convertView.findViewById(R.id.client_name))
+                    .setText(currentDataMap.get("client_name").toString());
+
+            ((MLTextView) convertView.findViewById(R.id.client_event))
+                    .setText(currentDataMap.get("client_event").toString());
+
+            ((MLTextView) convertView.findViewById(R.id.event_time))
+                    .setText(currentDataMap.get("event_time").toString());
+
+            ((MLTextView) convertView.findViewById(R.id.client_net_speed))
+                    .setText(currentDataMap.get("client_net_speed").toString());
+
+
+
+            return convertView;
+        }
     }
 }

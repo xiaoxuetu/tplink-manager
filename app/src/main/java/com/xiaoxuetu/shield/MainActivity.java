83fb12e6db53bf2bb3473ab3f9e27bf5acc60921
kevin <hjj20040849@qq.com>
2017-02-16 00:32:57 +0800

    FEATURE: 添加按两次返回键退出功能

--- app/src/main/java/com/xiaoxuetu/shield/MainActivity.java
+++ app/src/main/java/com/xiaoxuetu/shield/MainActivity.java
@@ -1,10 +1,12 @@
 package com.xiaoxuetu.shield;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,6 +15,7 @@ import android.widget.ImageView;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.xiaoxuetu.shield.common.widget.dialog.MLTextView;
 import com.xiaoxuetu.shield.login.dao.RouteDao;
@@ -192,4 +195,42 @@ public class MainActivity extends AppCompatActivity {
             return convertView;
         }
     }
+
+
+    private boolean isExit = false;
+
+    @Override
+    public boolean onKeyUp(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {// 当keyCode等于退出事件值时
+            quit();
+            return false;
+        } else {
+            return super.onKeyDown(keyCode, event);
+        }
+    }
+
+    private void quit() {
+        if (isExit) {
+            // ACTION_MAIN with category CATEGORY_HOME 启动主屏幕
+            Intent intent = new Intent(Intent.ACTION_MAIN);
+            intent.addCategory(Intent.CATEGORY_HOME);
+            startActivity(intent);
+            System.exit(0);// 使虚拟机停止运行并退出程序
+        } else {
+            isExit = true;
+            Toast.makeText(MainActivity.this, "再按一次退出哦", Toast.LENGTH_SHORT).show();
+            mHandler.sendEmptyMessageDelayed(0, 3000);// 3秒后发送消息
+        }
+    }
+
+    //创建Handler对象，用来处理消息
+    private Handler mHandler = new Handler() {
+
+        @Override
+        public void handleMessage(Message msg) {//处理消息
+            // TODO Auto-generated method stub
+            super.handleMessage(msg);
+            isExit = false;
+        }
+    };
 }
--- app/src/main/java/com/xiaoxuetu/shield/login/LoginActivity.java
+++ app/src/main/java/com/xiaoxuetu/shield/login/LoginActivity.java
@@ -6,6 +6,7 @@ import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -148,4 +149,41 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
     }
+
+    private boolean isExit = false;
+
+    @Override
+    public boolean onKeyUp(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {// 当keyCode等于退出事件值时
+            quit();
+            return false;
+        } else {
+            return super.onKeyDown(keyCode, event);
+        }
+    }
+
+    private void quit() {
+        if (isExit) {
+            // ACTION_MAIN with category CATEGORY_HOME 启动主屏幕
+            Intent intent = new Intent(Intent.ACTION_MAIN);
+            intent.addCategory(Intent.CATEGORY_HOME);
+            startActivity(intent);
+            System.exit(0);// 使虚拟机停止运行并退出程序
+        } else {
+            isExit = true;
+            Toast.makeText(MainActivity.this, "再按一次退出哦", Toast.LENGTH_SHORT).show();
+            mHandler.sendEmptyMessageDelayed(0, 3000);// 3秒后发送消息
+        }
+    }
+
+    //创建Handler对象，用来处理消息
+    private Handler mHandler = new Handler() {
+
+        @Override
+        public void handleMessage(Message msg) {//处理消息
+            // TODO Auto-generated method stub
+            super.handleMessage(msg);
+            isExit = false;
+        }
+    };
 }
